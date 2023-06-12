e30e838a8a1ca071f6cc4e45f221d22ae48911e1
Nick Gerace <nickagerace@gmail.com>
2023-06-12 11:50:21 -0400

    Add chunks
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- script.sh
+++ script.sh
@@ -15,3 +15,15 @@ echo -e "Yay! It all worked! 👍\n"
 
 echo "BUILDKITE_PARALLEL_JOB: $BUILDKITE_PARALLEL_JOB"
 echo "BUILDKITE_PARALLEL_JOB_COUNT: $BUILDKITE_PARALLEL_JOB_COUNT"
+
+if [ "$BUILDKITE_PARALLEL_JOB" = "0" ]; then
+    echo "chunk 1 of 5: https://tinyurl.com/"
+elif [ "$BUILDKITE_PARALLEL_JOB" = "1" ]; then
+    echo "chunk 2 of 5: 3v"
+elif [ "$BUILDKITE_PARALLEL_JOB" = "2" ]; then
+    echo "chunk 3 of 5: 3h"
+elif [ "$BUILDKITE_PARALLEL_JOB" = "3" ]; then
+    echo "chunk 4 of 5: k8"
+elif [ "$BUILDKITE_PARALLEL_JOB" = "4" ]; then
+    echo "chunk 5 of 5: yn"
+fi
